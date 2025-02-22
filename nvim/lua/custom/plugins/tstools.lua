8b1d8938d60bb51fb0f233cf8ab01fc14ac086ed
Constantin Barac <ilconstantinbarac@gmail.com>
2025-02-22 12:52:42 +0200

    Init

---
+++ nvim/lua/custom/plugins/tstools.lua
@@ -0,0 +1,10 @@
+return {
+  'pmizio/typescript-tools.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  opts = {
+    settings = {
+      separate_diagnostic_server = false,
+      tsserver_max_memory = 8 * 1024,
+    },
+  },
+}
