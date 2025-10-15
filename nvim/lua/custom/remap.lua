f81715a125cc380ef197f47d123df5f75a32678f
Ilie-Constantin Barac <ibar@192.168.1.3>
2025-10-15 16:20:09 +0300

    only keep single line number impl

--- nvim/lua/custom/remap.lua
+++ nvim/lua/custom/remap.lua
@@ -22,18 +22,16 @@ vim.keymap.set({ 'n', 'v' }, '<leader>crp', function()
   local mode = vim.fn.mode()
   local location
   if mode == 'v' or mode == 'V' or mode == string.char(22) then
-    local start_line = vim.fn.line "'<"
-    local end_line = vim.fn.line "'>"
-    if start_line == end_line then
-      location = file_path .. ':' .. start_line
-    else
-      location = file_path .. ':' .. start_line .. '-' .. end_line
-    end
+    local line_number = vim.fn.line '.'
+    location = file_path .. ':' .. line_number
+    local esc = vim.api.nvim_replace_termcodes('<esc>', true, false, true)
+    vim.api.nvim_feedkeys(esc, 'x', false)
   else
     location = file_path
   end
   vim.fn.setreg('+', location)
-end, { desc = 'Copy relative path (with line numbers in visual mode)' })
+  print('Copied: ' .. location)
+end, { desc = 'Copy relative path (with line number in visual mode)' })
 
 vim.keymap.set({ 'n', 'v' }, '<leader>d', '"_d')
 
