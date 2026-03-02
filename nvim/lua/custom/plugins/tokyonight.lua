ecb90235014fdb641452cd5342434ae1f014d4a2
Ilie-Constantin Barac <ibar@192.168.1.5>
2026-03-02 13:38:14 +0200

    update theme

--- nvim/lua/custom/plugins/tokyonight.lua
+++ nvim/lua/custom/plugins/tokyonight.lua
@@ -3,6 +3,11 @@ return {
   priority = 1000,
   opts = {
     style = 'night',
+    transparent = true,
+    styles = {
+      sidebars = 'transparent',
+      floats = 'transparent',
+    },
   },
   init = function()
     vim.cmd.colorscheme 'tokyonight'
